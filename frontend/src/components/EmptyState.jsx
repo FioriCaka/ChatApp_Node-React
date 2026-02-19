071d57d9e4a9f7702605cf0ad065f015379a85ea
FioriCaka <fioricaka228@gmail.com>
2026-02-19 13:25:13 +0100

    feat: update EmptyState component to include profile editing functionality

--- frontend/src/components/EmptyState.jsx
+++ frontend/src/components/EmptyState.jsx
@@ -1,23 +1,30 @@
-function EmptyState({ userFirstName, onOpenSidebar }) {
+function EmptyState({ userFirstName, onOpenSidebar, onOpenProfile }) {
   return (
-    <div
-      className="flex flex-col justify-start mb-4"
-      style={{ backgroundImage: "url('/balloon-background.jpg')" }}
-    >
-      <button
-        className="rounded-xl border relative bg-white/30 border-white/60 px-3 py-2 h-12 w-12 text-sm lg:hidden top-10 left-3 backdrop-blur-3xl shadow-lg shadow-black/10"
-        type="button"
-        onClick={onOpenSidebar}
-      >
-        <img src="/menu-outline.svg" alt="" className="h-6 w-6" />
-      </button>
-      <div className="mt-12 text-center">
-        <h2 className="text-2xl font-semibold">
-          Welcome back, {userFirstName}
-        </h2>
-        <p className="text-slate-500">
-          Pick a contact or chat to start messaging.
-        </p>
+    <div className="flex flex-col justify-start mb-4">
+      <div className="mt-12">
+        <div className="mt-12 text-center">
+          <h2 className="text-2xl font-semibold">
+            Welcome back, {userFirstName}
+          </h2>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center self-center gap-3 mt-48">
+        <button
+          className="rounded-xl border flex flex-col items-center justify-center relative bg-white/30 border-white/60 px-3 py-2 h-fit w-36 text-sm lg:hidden backdrop-blur-3xl shadow-lg shadow-black/10"
+          type="button"
+          onClick={onOpenSidebar}
+        >
+          <img src="/chatbubble.svg" alt="" className="h-12 w-12" />
+          <p>Send a message</p>
+        </button>
+        <button
+          className="rounded-xl border flex flex-col items-center justify-center relative bg-white/30 border-white/60 px-3 py-2 h-fit w-36 text-sm lg:hidden backdrop-blur-3xl shadow-lg shadow-black/10"
+          type="button"
+          onClick={onOpenProfile}
+        >
+          <img src="/create-outline.svg" alt="" className="h-12 w-12" />
+          <p>Edit profile</p>
+        </button>
       </div>
     </div>
   );
